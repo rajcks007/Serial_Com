34b61d311a80fb7eff5815c56d2d772f49fee449
raj choksi <rajchoksi97@gmail.com>
2024-04-03 18:57:15 +0200

    serial_cofig

---
+++ main/serial_comunication_UART.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "driver/uart.h"
+
+#define TXD_PIN 43
+#define RXD_PIN 44
+
+#define RX_BUF_SIZE 1024 // Buffer size
+
+void app_main(void)
+{
+    
+    // for (int i=0;i<10;i++)
+    // printf("Hello world!\n");
+
+    uart_config_t uart_config = {
+
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+    
+    };
+
+    uart_param_config(UART_NUM_0, &uart_config); //Passing uart port and config
+    uart_set_pin(UART_NUM_0, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE); //set uart pin (tx:, rx:, RTS:, CTS: )
+    uart_driver_install(UART_NUM_0,RX_BUF_SIZE, 0, 0, NULL, 0); //UART_Port, rx buff size, queue size, flag
+
+
+    
+
+}
