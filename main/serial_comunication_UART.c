a128d6fa422b0c9927d5b8a09b2572e8f07c3d90
raj choksi <rajchoksi97@gmail.com>
2024-04-04 11:32:59 +0200

    UART io add & buffer clear

--- main/serial_comunication_UART.c
+++ main/serial_comunication_UART.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include "driver/uart.h"
+#include "esp_log.h"
+#include "string.h"
 
 
-#define TXD_PIN 43
-#define RXD_PIN 44
+#define TXD_PIN 43  //   13
+#define RXD_PIN 44  //  12
 
-#define RX_BUF_SIZE 1024 // Buffer size
+#define RX_BUF_SIZE 512 // Buffer size
 
 void app_main(void)
 {
@@ -29,11 +31,12 @@ void app_main(void)
     for (int i = 0; i < 10; i++)
     {
         char message[] = "RAJ \n";
-        printf("Sending : %s", message);
         uart_write_bytes(UART_NUM_0,message, sizeof(message));
 
         char incomming_message [RX_BUF_SIZE];
+        memset(incomming_message,0,sizeof(incomming_message));
         uart_read_bytes(UART_NUM_0,(uint8_t *)incomming_message, RX_BUF_SIZE,pdMS_TO_TICKS(1000));
+        printf("Recive : %s \n",incomming_message);
     }
 
 
